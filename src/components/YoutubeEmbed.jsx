90412d95dd2203b08dbcff3f8b4264b601d09867
limouzeen <amaratkos@hotmail.com>
2025-04-04 01:43:55 +0700

    fix iframe

--- src/components/YoutubeEmbed.jsx
+++ src/components/YoutubeEmbed.jsx
@@ -1,3 +1,4 @@
+// src/components/YoutubeEmbed.jsx
 import React from "react";
 import { Box } from "@mui/material";
 
@@ -17,7 +18,7 @@ export default function YoutubeEmbed() {
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/QVRKydvntcA?autoplay=1&mute=1"
+        src="https://www.youtube.com/embed/OfQnNHGN69E?autoplay=1&mute=1&loop=1&playlist=OfQnNHGN69E"
         title="YouTube video player"
         frameBorder="0"
         allow="autoplay; encrypted-media"
